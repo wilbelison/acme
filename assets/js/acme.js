cff65e0d533c7fba74e96ad171a9234946fb64d8
Wilbelison Junior <meustudio@gmail.com>
2019-04-11 19:44:12 -0300

    local scripts

--- assets/js/acme.js
+++ assets/js/acme.js
@@ -1,78 +1,71 @@
-function init() {
+$(document).ready(
+	function(){
 
-	$(document).ready(
-		function(){
+		/*---------- MUSTACHE ----------*/
 
-			/*---------- MUSTACHE ----------*/
+		var templateColaboradores = $('#template-colaborador').html();
+		var templateDetalhes = $('#template-detalhes').html();
 
-			var templateColaboradores = $('#template-colaborador').html();
-			var templateDetalhes = $('#template-detalhes').html();
+		$.getJSON('https://randomuser.me/api/?results=10&nat=br&inc=info,picture,name,email,cell,location', function(data) {
+			
+			var view = data.results.map((e, i) => { return {colaborador: e, index: i} });
 
-			$.getJSON('https://randomuser.me/api/?results=20&nat=br&inc=info,picture,name,email,cell,location&seed=abc', function(data) {
-				
-				var view = data.results.map((e, i) => { return {colaborador: e, index: i} });
+			var htmlColaboradores = Mustache.render(templateColaboradores, view);
+			var htmlDetalhes = Mustache.render(templateDetalhes, view);
 
-				var htmlColaboradores = Mustache.render(templateColaboradores, view);
-				var htmlDetalhes = Mustache.render(templateDetalhes, view);
+			$('#colaboradores').html(htmlColaboradores);
+			$('body').append(htmlDetalhes);
 
-				$('#colaboradores').html(htmlColaboradores);
-				$('body').append(htmlDetalhes);
+		});
 
-			});
+		/*---------- PESQUISAR ----------*/
 
-			/*---------- PESQUISAR ----------*/
+		$('#pesquisar').on('click', function() {
+			$('#query').focus();
+		});
 
-			$('#pesquisar').on('click', function() {
-				$('#query').focus();
-			});
+		$('#query').on('input', function() {
 
-			$('#query').on('input', function() {
+			var value = $(this).val();
 
-				var value = $(this).val();
+			$('#pesquisar').off('click');
 
-				$('#pesquisar').off('click');
+			if(value.length >= 1) {
 
-				if(value.length >= 1) {
+				$('#pesquisar').addClass('limpar');
 
-					$('#pesquisar').addClass('limpar');
-
-					$('#pesquisar').on('click', function() {
-
-						$('#pesquisar').removeClass('limpar');
-
-						$('#query').val('');
-						$('#query').blur();
-
-						$('#pesquisar').off('click');
-
-						$('#pesquisar').on('click', function() {
-							$('#query').focus();
-						});
-
-					});
-
-				} else {
+				$('#pesquisar').on('click', function() {
 
 					$('#pesquisar').removeClass('limpar');
+
+					$('#query').val('');
+					$('#query').blur();
+
+					$('#pesquisar').off('click');
 
 					$('#pesquisar').on('click', function() {
 						$('#query').focus();
 					});
 
-				}
+				});
 
-			});
+			} else {
 
-		}
-	);
+				$('#pesquisar').removeClass('limpar');
 
-	/* exibe e oculta detalhes */
+				$('#pesquisar').on('click', function() {
+					$('#query').focus();
+				});
 
-	detalhes = function(id) {
-		$(id).toggleClass('oculto');
+			}
+
+		});
+
 	}
+);
 
+/* exibe e oculta detalhes */
+
+var detalhes = function(id) {
+	$(id).toggleClass('oculto');
 }
-
-var detalhes;
-
